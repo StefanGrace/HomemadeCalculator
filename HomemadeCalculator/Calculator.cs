38727b07d8dd74d9bf713b63c4accb1736bf9f98
StefanGrace <stefangrace@hotmail.co.nz>
2021-12-06 00:51:44 +1300

    Add files via upload

---
+++ HomemadeCalculator/Calculator.cs
@@ -0,0 +1,53 @@
+﻿namespace HomemadeCalculator
+{
+    class Calculator
+    {
+        /**
+         * Homemade calculator using only subtraction. 
+         * Can perform addition, subtraction, multiplication, division (rounds up), power.
+         * Multiplication, division and power only work on non-negative integers.
+         */
+
+        public int minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int add(int a, int b)
+        {
+            return minus(a, (minus(0, b)));
+        }
+
+        public int multiply(int a, int b)
+        {
+            int c = 0;
+            for (int i = 0; i < b; i = add(i, 1))
+            {
+                c = add(c, a);
+            }
+            return c;
+        }
+
+        public int divide(int a, int b)
+        {
+            int c = a;
+            int i = 0;
+            while (c > 0)
+            {
+                c = minus(c, b);
+                i = add(i, 1);
+            }
+            return i;
+        }
+
+        public int power(int a, int b)
+        {
+            int c = 1;
+            for (int i = 0; i < b; i = add(i, 1))
+            {
+                c = multiply(c, a);
+            }
+            return c;
+        }
+    }
+}
---
+++ HomemadeCalculator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace HomemadeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "";
+            while (choice != "0")
+            {
+                Console.WriteLine("0 - Exit\n" +
+                    "1 - Add\n" +
+                    "2 - Minus\n" +
+                    "3 - Multiply\n" +
+                    "4 - Divide\n" +
+                    "5 - Power");
+                Console.Write("Enter choice: ");
+                choice = Console.ReadLine();
+
+                int a = 0;
+                int b = 0;
+                switch (choice)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                        try
+                        {
+                            Console.Write("Enter first number: ");
+                            a = int.Parse(Console.ReadLine());
+                            if (a < 0)
+                            {
+                                Console.WriteLine("Can't do negatives");
+                                choice = "E";
+                                break;
+                            }
+                            Console.Write("Enter second number: ");
+                            b = int.Parse(Console.ReadLine());
+                            if (b < 0)
+                            {
+                                Console.WriteLine("Can't do negatives");
+                                choice = "E";
+                                break;
+                            }
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Must enter an integer");
+                            choice = "E";
+                        }
+                        break;
+                }
+                
+                Calculator calc = new Calculator();
+                int result = 0;
+
+                switch (choice)
+                {
+                    case "E": break;
+                    case "0": Console.WriteLine("Goodbye");  break;
+                    case "1": result = calc.add(a, b); break;
+                    case "2": result = calc.minus(a, b); break;
+                    case "3": result = calc.multiply(a, b); break;
+                    case "4": result = calc.divide(a, b); break;
+                    case "5": result = calc.power(a, b); break;
+                    default: Console.WriteLine("Invalid choice: " + choice); break;
+                }
+
+                switch (choice)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                        Console.WriteLine("Result: " + result);
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
